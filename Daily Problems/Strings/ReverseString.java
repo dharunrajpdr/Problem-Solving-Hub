cdb49e840135ad0a2566a5fa3e94d0caa14dea83
Dharunraj P <pdharunraj@gmail.com>
2026-09-24 10:27:52 +0530

    Add reverseString method to Solution class
    Implemented a method to reverse a list of characters.

---
+++ Daily Problems/Strings/ReverseString.java
@@ -0,0 +1,12 @@
+
+// Input : s = ["h", "e" ,"l" ,"l" ,"o"]
+// Output : ["o", "l", "l", "e", "h"]
+
+// Input : s = ["b", "y" ,"e" ]
+// Output : ["e", "y", "b"]
+
+class Solution {
+    public void reverseString(List<Character> s) {
+        Collections.reverse(s);
+    }
+}
