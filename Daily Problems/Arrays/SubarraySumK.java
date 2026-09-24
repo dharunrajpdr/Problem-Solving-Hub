b22653bff3df4b5bd218a6a5ff4ecd91a41a4e36
Dharunraj P <pdharunraj@gmail.com>
2026-09-24 09:55:35 +0530

    Add optimal subarray sum implementation using prefix sum
    Implemented an optimal solution for subarray sum using prefix sums and HashMap.

--- Daily Problems/Arrays/SubarraySumK.java
+++ Daily Problems/Arrays/SubarraySumK.java
@@ -22,3 +22,29 @@ class Main {
         return count;
     }
 }
+
+//optimal (using prefixsum)
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int[] arr ={1,2,3};
+        int k=3;
+        System.out.println(subarraySum(arr,k));
+    }
+    public static int subarraySum(int[] arr,int k){
+        int n=arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);                               //key:prefixSum,value:count
+        int presum=0;
+        int count=0;
+        for(int num:arr){
+            presum+=num;
+            if(map.containsKey(presum-k)){
+                count+=map.get(presum-k);
+            }
+            map.put(presum,map.getOrDefault(presum,0)+1);
+        }
+        return count;
+    }
+}
