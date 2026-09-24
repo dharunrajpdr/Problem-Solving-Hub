127892f9d03e5fec36ece0827c464bba121c9879
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-09-24 19:52:36 +0530

    Comment out brute force implementation in SubarraySumK.java

--- Daily Problems/Arrays/SubarraySumK.java
+++ Daily Problems/Arrays/SubarraySumK.java
@@ -1,27 +1,27 @@
 //Brute force 
-class Main {
-    public static void main(String[] args) {
-        int[] arr ={1,2,3};
-        int k=3;
-        System.out.println(subarraySum(arr,k));
-    }
-    public static int subarraySum(int[] arr,int x){
-        int count=0;
-        int n=arr.length;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                int sum=0;
-                for(int k=i;k<=j;k++){
-                    sum+=arr[k];
-                }
-                if(sum==x){
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-}
+// class Main {
+//     public static void main(String[] args) {
+//         int[] arr ={1,2,3};
+//         int k=3;
+//         System.out.println(subarraySum(arr,k));
+//     }
+//     public static int subarraySum(int[] arr,int x){
+//         int count=0;
+//         int n=arr.length;
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 int sum=0;
+//                 for(int k=i;k<=j;k++){
+//                     sum+=arr[k];
+//                 }
+//                 if(sum==x){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
 
 //optimal (using prefixsum)                                                  T.C: O(n*log n) S.C: O(n)
 
